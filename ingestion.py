93ae740df0bb85185bf6fd01730017a07c210539
YassinfromMorocco <rouani.yassin@gmail.com>
2023-09-17 23:09:15 +0100

    :update to last version

--- ingestion.py
+++ ingestion.py
@@ -1,9 +1,11 @@
 from config import SOURCES
 from utils import get_schema_from_dict
+import os
+
 
 def run_cockpit_sfr_data_ingestion(path_name: str, bucket: str):
     print(f" ETL cockpit_sft triggered")
-    file_name = path_name.split("/")[-1]
+    file_name, file_extension = os.path.splitext(path_name.split("/")[-1])
     print(f" bucket is {bucket}")
     print(f" file name is {file_name}")
     get_schema_from_dict(file_name=file_name, bucket_name=bucket)
